8dd008def99ed0e296440db3e418b02dba05dc2a
nata1976elya1976@gmail.com <nata1976elya1976@gmail.com>
2020-06-24 16:44:38 +0300

    Добавьте файлы проекта.

---
+++ Ychet/Menu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ychet
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenSupplies_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonOpenClients_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonAgents_Click(object sender, EventArgs e)
+        {
+            Form agentSet = new Agents();
+            agentSet.Show();
+        }
+
+        private void buttonProvider_Click(object sender, EventArgs e)
+        {
+            Form agentSet = new AgentsSet();
+            agentSet.Show();
+        }
+
+        private void buttonExtra_Click(object sender, EventArgs e)
+        {
+            Form agentSet = new AgentsSet();
+            agentSet.Show();
+        }
+
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            Form agentSet = new AgentsSet();
+            agentSet.Show();
+        }
+
+        private void buttonNal_Click(object sender, EventArgs e)
+        {
+            Form agentSet = new AgentsSet();
+            agentSet.Show();
+        }
+
+        private void buttonPriem_Click(object sender, EventArgs e)
+        {
+            Form agentSet = new AgentsSet();
+            agentSet.Show();
+        }
+    }
+}
